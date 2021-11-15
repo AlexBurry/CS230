1d60090d13b751013855a095e88a6159b40523f8
Marcus Chung <72574748+MarcusChung@users.noreply.github.com>
2021-11-15 20:32:18 +0000

    Create Rat class

---
+++ src/Rat.java
@@ -0,0 +1,15 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Rat {
+
+    public static void main(String[] args) {
+        System.out.println("Start of rats class");
+    }
+
+
+    @Override
+    public String toString() {
+        return "";
+    }
+    
+}
