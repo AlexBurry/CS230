82fdba6ccfca2c43fd620228746f49c1a864d15e
Marcus Chung <72574748+MarcusChung@users.noreply.github.com>
2021-11-19 12:24:57 +0000

    Added new variables

--- src/RAT FILES/Rat.java
+++ src/RAT FILES/Rat.java
@@ -1,6 +1,28 @@
 import javax.sound.sampled.SourceDataLine;
 
+/**
+ * The main rat class. Covers all basic features of Rats.
+ * @author Marcus
+ * @author Iggy
+ * @version 0.1
+ * @since 0.1
+ */
+
 public class Rat {
+
+    private Boolean sex;
+    private Boolean isBaby;
+    private Boolean isDeath;
+    private Boolean alive;
+    private Boolean isSterile;
+
+    public Rat(Boolean sex, Boolean isBaby, Boolean isDeath, Boolean alive, Boolean isSterile) {
+        this.sex = sex;
+        this.isBaby = isBaby;
+        this.isDeath = isDeath;
+        this.alive = alive;
+        this.isSterile = isSterile;
+    }
 
     public static void main(String[] args) {
         System.out.println("Start of rats class");
