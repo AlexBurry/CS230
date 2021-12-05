ed409b92a19f26eac8b070ae77d8367e377e4ccf
Joeli1324 <94002218+Joeli1324@users.noreply.github.com>
2021-12-05 18:46:19 +0000

    Update LeaderBoard.java

--- src/GUI/LeaderBoard.java
+++ src/GUI/LeaderBoard.java
@@ -1,4 +1,4 @@
-package GUI;
+package GUI:
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -9,14 +9,14 @@ import java.util.List;
 
 public class LeaderBoard {
 
-    private int currentLevel;
+	private int currentLevel;
 
-    public void endGame(int finalScore) throws IOException{
+	public void endGame(int finalScore) throws IOException{
     List<String> scores = retrieveScores();
     showLeaderBoard(scores);
 }
 
-    private List<String> retrieveScores() throws IOException {
+	private List<String> retrieveScores() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("Lvl".concat(String.valueOf(currentLevel).concat(".txt"))));
         String scoreLine = reader.readLine(); // read line that contains scores
         List<String> scores = new ArrayList<>();
@@ -27,12 +27,19 @@ public class LeaderBoard {
         reader.close();
         return scores;
     }
-
+    
     private void showLeaderBoard(List<String> scores) throws IOException{
-        System.out.println("*** LEADERBOARD ***");
+        System.out.println("* LEADERBOARD *");
             for(int i=0; i< scores.size(); i++){
+            	for (int j = i+1; j < 11 ; j++) {
+              	 if(scores.get(i) + 1 < scores.get(j) + 1) {
+                   temp = arr[i];
+                   arr[i] = arr[j];
+                   arr[j] = temp;
+               }
+            }
                 System.out.println(scores.get(i));
         }
-        System.out.println("*** LEADERBOARD ***");
+        System.out.println("* Highscore Board *");
     }
 }
